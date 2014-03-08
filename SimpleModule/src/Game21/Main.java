27fc83dca287a0869f0ef07708edf6e86aa12f56
MihailMecetin <mihailmecetin@gmail.com>
2014-03-08 12:33:20 +0200

    21

--- SimpleModule/src/Game21/Main.java
+++ SimpleModule/src/Game21/Main.java
@@ -10,40 +10,62 @@ public class Main {
     public static void main(String[] args) {
         User U = new User();
         Comp C = new Comp();
+        C.LastWon = true;
 
 
-        C.SetStones();
-        System.out.println(C.GetStones());
-
+        do{
         Turn (C,U);
 
+        }while (! ((C.MaxStones == 0) || (U.MaxStones == 0))  );
 
-
-
-
-
-
-
+        if(C.MaxStones == 0){
+            System.out.println("Comp Vijgral igru!");
+        }
+        if(U.MaxStones == 0){
+            System.out.println("Ti Vijgral igru!");
+        }
 
     }
 
 
-    public static void Turn (Player C,Player U) {
+    public static void Turn (Comp C,User U) {
         int Summ;
-        ((Comp)C).SetStones();
-        ((User)U).SetStones();
-        Summ = ((Comp)C).Stones  +  ((User)U).Stones;
-        System.out.println(Summ);
 
+        //Vibiraem summu
+        C.SetStones();
+        U.SetStones();
+        Summ = C.Stones  +  U.Stones;
+        System.out.println("Vasha summa = "+Summ);
 
-
-
+        //Gadaem summu
+        if(C.LastWon){
+            C.Guess(U,0);
+            U.Guess(C.Guess);
+        }
+        else{
+            U.Guess(0);
+            C.Guess(U,U.Guess);
+        }
+        if(Summ == C.Guess){
+            C.MaxStones--;
+            System.out.println("Comp vijgral raund.U nego teperj "+C.MaxStones+" kamnej");
+        }
+        else if(Summ == U.Guess){
+            U.MaxStones--;
+            System.out.println("Ti vijgral raund.U tebja teperj "+U.MaxStones+" kamnej");
+            C.LastWon = !C.LastWon;
+        }
+        else {
+            System.out.println("Nikto ne vijgral raund");
+        }
 
     }
 
     public static class Player {
         int Stones = 0;
         int MaxStones = 3;
+        int Guess = 0;
+        boolean LastWon;
         public void SetStones(int i){
            this.Stones=i;
         }
@@ -56,20 +78,44 @@ public class Main {
         public void SetStones(){
             System.out.println("Viberi Chislo ot 0 do "+MaxStones);
 
-            Scanner sc = new Scanner(System.in);
-            int i = sc.nextInt();
+            int i;
+
+            do{
+                Scanner sc = new Scanner(System.in);
+                i = sc.nextInt();
+                if  ( (i>MaxStones) || (i<0) ){
+                    System.out.print("Ti Idiot? Poprobuj Snova");
+                }
+            }while( (i>MaxStones) || (i<0) );
+
 
             System.out.println("Ti Vibral "+i);
             Stones=i;
         }
-        public int Guess(){
+        public void Guess(int Taken){
             System.out.println("Skolko U vas kamnej vmeste?");
-            Scanner sc = new Scanner(System.in);
-            int i = sc.nextInt();
 
-            System.out.println("Ti Dumaesh u vas "+i);
+            int i;
 
-            return i;
+            if(Taken != 0){
+                System.out.println("Nelzja ugadivatj "+Taken);
+
+                do{
+                    Scanner sc = new Scanner(System.in);
+                    i = sc.nextInt();
+                    if  (i == Taken){
+                        System.out.print("Ti Idiot? Poprobuj Snova");
+                    }
+                }while( i==Taken );
+            }
+            else{
+                Scanner sc = new Scanner(System.in);
+                i = sc.nextInt();
+            }
+
+
+            System.out.println("Ti Dumaesh u vas Vmeste "+i);
+            Guess =  i;
         }
     }
 
@@ -80,10 +126,16 @@ public class Main {
             System.out.println("Comp vibral "+RandomInt);
             Stones = RandomInt;
         }
-        public int Guess(Player U){
+        public void Guess(Player U,int Taken){
             Random rr = new Random();
             int RandomInt = rr.nextInt(U.MaxStones+1);
-            return Stones+RandomInt;
+            if (Taken!=0) {
+                do{
+                    RandomInt = rr.nextInt(U.MaxStones+1);
+                }while (Stones+RandomInt == Taken);
+            }
+            Guess = Stones+RandomInt;
+            System.out.println("Comp Dumaet u vas vmeste "+Guess);
         }
     }
 
