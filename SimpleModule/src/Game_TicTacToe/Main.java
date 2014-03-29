fb74cd4a76aa9b2fe7f889f662b9e0fe0574e4ac
MihailMecetin <mihailmecetin@gmail.com>
2014-03-29 12:51:51 +0200

    qq

--- SimpleModule/src/Game_TicTacToe/Main.java
+++ SimpleModule/src/Game_TicTacToe/Main.java
@@ -1,14 +1,55 @@
 package Game_TicTacToe;
 
 
+import sun.org.mozilla.javascript.internal.ast.WhileLoop;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        new Game(new HumanPlayer(),new AIPlayer());
+
+
+
+
+
     }
 
-    public class Board{
+    static class Game {
+        private Board board = new Board();
+        private Player PlayerOne;
+        private Player PlayerTwo;
+        private Player CurrentPlayer;
+
+        public Game (Player p1,Player p2){
+            PlayerOne = p1;
+            PlayerTwo = p2;
+            CurrentPlayer = p1;
+
+            While(!gameover){
+
+
+            }
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+    boolean gameover(Board b){
+
+        return false;
+    }
+
+    static public class Board{
         int[][] board = new int[3][3];
+        int test;
         public boolean PlaceOnBoard(int x,int y,int token){
             if( board[x][y]!=0 ){
                 board[x][y]=token;
@@ -20,9 +61,14 @@ public class Main {
             }
         }
         public void PrintBoard(){
+            System.out.print(board[1]);
+            System.out.print(board[2]);
+            System.out.print(board[3]);
 
 
-
+        }
+        public int TestTest(){
+            return test;
         }
 
 
@@ -35,7 +81,7 @@ public class Main {
         public void doMove(Board b);
     }
 
-    public class HumanPlayer implements Player{
+    public static class HumanPlayer implements Player{
 
         @Override
         public void doMove(Board b) {
@@ -45,7 +91,7 @@ public class Main {
 
     }
 
-    public class AIPlayer implements Player{
+    public static class AIPlayer implements Player{
 
 
         public void doMove(Board b) {
